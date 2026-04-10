44912ed786a3aa552093431159d28ea8e959bf7a
Bastien-Antigravity <bastien.chanseau.bc@gmail.com>
2026-04-10 21:59:24 +0200

    fix(rust): fix GrpcServer trait bounds and types

--- rust/src/network/grpc_server.rs
+++ rust/src/network/grpc_server.rs
@@ -12,12 +12,12 @@ impl GrpcServer {
         Self { addr }
     }
 
-    pub async fn start<S, B>(self, service: S, descriptor_set: &'static [u8]) -> Result<(), Box<dyn std::error::Error>>
+    pub async fn start<S>(self, service: S, descriptor_set: &'static [u8]) -> Result<(), Box<dyn std::error::Error>>
     where
         S: tonic::server::NamedService + Clone + Send + 'static,
-        S: tonic::codegen::Service<http::Request<tonic::body::BoxBody>, Response = http::Response<B>> + Send + 'static,
-        B: tonic::codegen::Body + Send + 'static,
-        B::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send + 'static,
+        S: tonic::codegen::Service<tonic::codegen::http::Request<tonic::codegen::Body>, Response = tonic::codegen::http::Response<tonic::codegen::BoxBody>> + Send + 'static,
+        S::Future: Send + 'static,
+        S::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
     {
         println!("Toolbox: GRPC Server listening on {}", self.addr);
 
